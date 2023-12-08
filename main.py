b577eae7c1a9db81dd8cec5469f9de62acb1a266
iva <ivanela.lazarova@gmail.com>
2023-12-08 12:04:07 +0800

    1.Changes to Invoice class: - passed article as a parameter in the class constructor to clarify intent and to encapsulate all info the class needs to function within its context - Passed the dataframe to the class, so it doesn't have to access a global df variable outside its scope 2.Changes to Article: - passed df to Article class and it's methods as a parameter

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ from fpdf import FPDF
 df = pd.read_csv("articles.csv", dtype="str")
 
 class Article:
-    def __init__(self, article_id):
+    def __init__(self, article_id, df):
         self.article_id = article_id
         self.article_name = df.loc[df["id"] == article_id, "name"].squeeze()
         self.article_price = df.loc[df["id"] == article_id, "price"].squeeze()
 
-    def available(self):
+
+    def available(self, df):
         """checks if article is available """
-        availability = df.loc[df["id"] == self.article_id, "in stock"].squeeze()
+        availability = df.loc[
+            df["id"] == self.article_id, "in stock"].squeeze()
         return int(availability)
 
-    #def buy(self):
-       # """ reduce the number of stock when an item is bought"""
-       # df.loc[df["id"] == self.article_id, "in stock"] -= 1
-       # new_stock = self.stock - 1
-       #df.to_csv("articles.csv", index=False)
-       #return new_stock
 
 class Invoice:
-    def __init__(self,):
-        self.invoice_number = df.loc
-        [df["id"] == article.article_id, "id"].squeeze()
+    def __init__(self, article):
+        self.article = article
+        self.invoice_number = article.article_id
 
     def generate(self):
         pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -33,19 +29,19 @@ class Invoice:
         pdf.set_font(family="Times", size=16, style="B")
         pdf.cell(w=50, h=8, txt=f"Invoice nr: {self.invoice_number}", ln=1)
 
-        pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Article: {article.article_name}", ln=1)
+        pdf.set_font(family="Times", size=12, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: {self.article.article_name}", ln=1)
 
-        pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"Price: {article.article_price}", ln=1)
+        pdf.set_font(family="Times", size=12, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: {self.article.article_price}", ln=1)
 
         pdf.output("receipt.pdf")
 
 
 article_id = input("Enter the id of the product you want to buy:")
-article = Article(article_id=article_id)
-if article.available():
-    invoice = Invoice()
+article = Article(article_id=article_id, df=df)
+if article.available(df):
+    invoice = Invoice(article=article)
     invoice.generate()
 else:
     print("Article not in stock")
